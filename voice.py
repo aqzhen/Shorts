2ffaf1cb83d5288a555da5960c3fd99126fb9c66
Rayan Yu <48096933+rayan-yu@users.noreply.github.com>
2024-02-17 10:50:14 -0500

    text-to-voice clean-up

--- voice.py
+++ voice.py
@@ -1,28 +1,38 @@
-from elevenlabs.client import ElevenLabs
+# from elevenlabs.client import ElevenLabs
+from collections.abc import Iterable
+
 from elevenlabs import Voice, VoiceSettings, generate, voices, play, stream
 import os
 
-# client = ElevenLabs(api_key="6bdd0a207a431564f4075147c58d5caf")
-os.environ["ELEVEN_API_KEY"] = "6bdd0a207a431564f4075147c58d5caf"
+def text_to_voice(input) -> bytes | Iterable[bytes]:
+    # client = ElevenLabs(api_key="6bdd0a207a431564f4075147c58d5caf")
+    os.environ["ELEVEN_API_KEY"] = "6bdd0a207a431564f4075147c58d5caf"
 
-input_folder = 'input-text'
-output_folder = 'output-audio'
-text_filename = 'induction.txt'
-audio_filename = 'induction.mp3'
+    input_folder = 'input-text'
 
-text_file_path = os.path.join(input_folder, text_filename)
-audio_file_path = os.path.join(output_folder, audio_filename)
+    text_filename = '{}.txt'.format(input)
 
-with open(text_file_path, 'r') as text_file:
-  input_text = text_file.read()
+    text_file_path = os.path.join(input_folder, text_filename)
 
-audio = generate(text=input_text,
-                 api_key='6bdd0a207a431564f4075147c58d5caf',
-                 voice=Voice(voice_id='EXAVITQu4vr4xnSDxMaL',
-                             settings=VoiceSettings(stability=0.71, similarity_boost=0.5, style=0.0, use_speaker_boost=True)),
-                 model="eleven_multilingual_v2")
+    with open(text_file_path, 'r') as text_file:
+      input_text = text_file.read()
 
+    return generate(text=input_text,
+                     api_key='6bdd0a207a431564f4075147c58d5caf',
+                     voice=Voice(voice_id='EXAVITQu4vr4xnSDxMaL',
+                                 settings=VoiceSettings(stability=0.71, similarity_boost=0.5, style=0.0, use_speaker_boost=True)),
+                     model="eleven_multilingual_v2")
+
+def write_to_file(audio, output):
+    output_folder = 'output-audio'
+    audio_filename = '{}.mp3'.format(output)
+    audio_file_path = os.path.join(output_folder, audio_filename)
+
+    with open(audio_file_path, "wb") as audio_file:
+        audio_file.write(audio)
+
+audio = text_to_voice('induction')
 play(audio)
+write_to_file(audio, 'induction')
 
-with open(audio_file_path, "wb") as audio_file:
-  audio_file.write(audio)
+
